a074e08c257458fbe9190f11e6bac25cc500bdfc
d_jingle <d_jingle@163.com>
2018-12-06 10:44:12 +0800

    second commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-      <van-nav-bar title="首页">
+  <div class="container">
+      <van-nav-bar title="首页" class="head">
           <van-icon name="search" slot="left"></van-icon>
           <van-icon name="cart" slot="right"></van-icon>
       </van-nav-bar>
@@ -23,7 +23,17 @@
               </swiper-slide>
           </swiper>
       </div>
-
+      <!--推荐商品-->
+      <div class="variety">
+          <p class="variety-title">推荐商品</p>
+          <div class="variety-swiper">
+              <div class="variety-swiper-content" v-for="(item , index) in varietyItem" :key=index >
+                  <img :src="item.img" alt="">
+                  <div>{{ item.name }}</div>
+                  <div>¥{{ item.price }}</div>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -161,6 +171,121 @@ export default{
                     city: "济宁市"
                 }
             ],
+            // 推荐商品
+            varietyItem: [
+                {
+                    name: "   汽油动力手推割杆机 养殖场专用青储割晒机",
+                    img:
+                        "http://img47.nongjx.com/2/20180628/636657846366213316507_198_170_5.jpg",
+                    price: "3260",
+                    company: "  曲阜市润众机械制造有限",
+                    city: "曲阜市"
+                },
+                {
+                    name: "   手扶带牧草收割机 青储高杆作物割晒机价格",
+                    img:
+                        "http://img47.nongjx.com/2/20180628/636657846396789904965_198_170_5.jpg",
+                    price: "3260",
+                    company: "  曲阜市润众机械制造有限",
+                    city: "曲阜市"
+                },
+                {
+                    name: "   不缠秧的花生果子收割机 手扶马铃薯收获机",
+                    img:
+                        "http://img48.nongjx.com/2/20180628/636657836692623289517_198_170_5.jpg",
+                    price: "2800",
+                    company: "  曲阜市润众机械制造有限",
+                    city: "曲阜市"
+                },
+                {
+                    name: "   高港区手扶车带动土豆收获机收山芋机器",
+                    img:
+                        "http://img49.nongjx.com/2/20180511/636616318740584319169_198_170_5.jpg",
+                    price: "1900",
+                    company: "  山东曲阜宏燊工贸有限公",
+                    city: "曲阜市"
+                },
+                {
+                    name: "   拖拉机带多功能割晒机 玉米高粱杆子收割机",
+                    img:
+                        "http://img50.nongjx.com/2/20180628/636657846333452686281_198_170_5.jpg",
+                    price: "3260",
+                    company: "  曲阜市润众机械制造有限",
+                    city: "曲阜市"
+                },
+                {
+                    name: "   厂家直销大型花生收获机 地下薯类挖收机",
+                    img:
+                        "http://img48.nongjx.com/2/20180628/636657834205779466463_198_170_5.jpg",
+                    price: "2800",
+                    company: "  曲阜市润众机械制造有限",
+                    city: "曲阜市"
+                },
+                {
+                    name: "   地瓜土豆杀秧机",
+                    img:
+                        "http://img49.nongjx.com/2/20180906/636718188965915110193_198_170_5.jpg",
+                    price: "2380",
+                    company: "  曲阜市汇林机械有限公司",
+                    city: "曲阜市"
+                },
+                {
+                    name: "   地瓜杀秧机",
+                    img:
+                        "http://img49.nongjx.com/2/20180906/636718188987287247797_198_170_5.jpg",
+                    price: "2380",
+                    company: "  曲阜市汇林机械有限公司",
+                    city: "曲阜市"
+                },
+                {
+                    name: "   自走式大型花生去秧机 花生除秧自动装袋机",
+                    img:
+                        "http://img48.nongjx.com/2/20180715/636672620827383254579_198_170_5.jpg",
+                    price: "2582",
+                    company: "  曲阜金源机械设备有限公",
+                    city: "济宁市"
+                },
+                {
+                    name: "   高效省人工花生摘果机 全新多功能去秧机",
+                    img:
+                        "http://img48.nongjx.com/2/20180715/636672620827383254579_198_170_5.jpg",
+                    price: "2581",
+                    company: "  曲阜金源机械设备有限公",
+                    city: "济宁市"
+                },
+                {
+                    name: "   河北邢台花生摘果机 自动装袋脱果机图片",
+                    img:
+                        "http://img48.nongjx.com/2/20180715/636672620803202789496_198_170_5.jpg",
+                    price: "2580",
+                    company: "  曲阜金源机械设备有限公",
+                    city: "济宁市"
+                },
+                {
+                    name: "   大型花生收获机规格 链条式方便花生挖果机",
+                    img:
+                        "http://img47.nongjx.com/2/20180926/636735768787313154613_198_170_5.jpg",
+                    price: "3260",
+                    company: "  曲阜浩发机械有限公司",
+                    city: "济宁市"
+                },
+                {
+                    name: "   家用节能花生摘果机 自动装袋花生分离机",
+                    img:
+                        "http://img50.nongjx.com/2/20180926/636735749293114513986_198_170_5.jpg",
+                    price: "3460",
+                    company: "  曲阜浩发机械有限公司",
+                    city: "济宁市"
+                },
+                {
+                    name: "   牵引式苞米杆打包机生产定制",
+                    img:
+                        "http://img67.nongjx.com/2/20170627/636341765014077992756_198_170_5.jpg",
+                    price: "面议",
+                    company: "  曲阜四达机械设备有限公",
+                    city: "济宁市"
+                }
+            ],
             swiperOption: {
                 slidesPerView: 3
             }
@@ -174,6 +299,16 @@ export default{
 </script>
 
 <style lang="scss">
+.head{
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 999!important;
+}
+.container{
+    background-color: #eeeeee;
+    margin-bottom: 1rem;
+}
 
 .carousel{
     height: 3rem;
@@ -185,13 +320,16 @@ export default{
     }
 }
 .hot{
+    background-color: white;
     &-title{
+        text-align: center;
         width: 100%;
         height: 0.5rem;
         padding-left: 0.2rem;
         line-height: 0.5rem;
         /*怪异盒模型，防止padding产生的边缘溢出*/
         box-sizing: border-box;
+        margin-top: 0.2rem;
     }
     &-swiper{
         &-content{
@@ -200,6 +338,38 @@ export default{
             img{
                 width: 2rem;
                 height: 2rem;
+            }
+        }
+    }
+}
+.variety{
+    background-color: white;
+    &-title{
+        text-align: center;
+        width: 100%;
+        height: 0.5rem;
+        padding-left: 0.2rem;
+        line-height: 0.5rem;
+        /*怪异盒模型，防止padding产生的边缘溢出*/
+        box-sizing: border-box;
+        margin-top: 0.2rem;
+    }
+    &-swiper{
+        display: flex;
+        /*设置子元素是否自动换行*/
+        flex-wrap: wrap;
+        /*设置空白区域显示*/
+        justify-content: space-around;
+        /*文字水平居中*/
+        text-align: center;
+        &-content{
+            /*子元素宽度占父元素45%*/
+            flex-basis: 45%;
+            /*子元素垂直居中*/
+            align-items: center;
+            img{
+                height: 2rem;
+                width: 2rem;
             }
         }
     }
